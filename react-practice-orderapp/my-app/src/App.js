3f8b51281ba3bc78c4acd750aa487fde0ed75a68
Xiuyue <zxy0722nn@gmail.com>
2023-06-01 11:17:58 +1200

    new practice burger-order app

---
+++ react-practice-orderapp/my-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{width:'750rem', height:200, backgroundColor:'#bfa'}}>
+    </div>
+  );
+}
+
+export default App;
