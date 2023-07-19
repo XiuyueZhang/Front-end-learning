74003f4b08e49c5ff299e03f3e886eb15ffed62c
Xiuyue <zxy0722nn@gmail.com>
2023-07-20 11:21:47 +1200

    redux set up

--- mern-project/social_media_app/client/src/App.js
+++ mern-project/social_media_app/client/src/App.js
@@ -2,10 +2,8 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
 import Homepage from "scenes/homePage";
 import Loginpage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
-import { useState } from "react";
 
 function App() {
-  const mode = useState("bright")
 
   return (
     <div className="app">
