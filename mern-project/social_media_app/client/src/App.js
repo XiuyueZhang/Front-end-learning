202b5be96d5c3ec4e91b66c23473e915a3379d53
Xiuyue <zxy0722nn@gmail.com>
2023-07-20 10:58:19 +1200

    set up front-end

--- mern-project/social_media_app/client/src/App.js
+++ mern-project/social_media_app/client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom"
+import Homepage from "scenes/homePage";
+import Loginpage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+import { useState } from "react";
 
 function App() {
+  const mode = useState("bright")
+
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Loginpage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
